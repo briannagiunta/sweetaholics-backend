e95e758746b311263ca65607fc802414e8546505
Brianna Giunta <brig_oowee@Briannas-MacBook-Pro.local>
2021-05-19 12:36:01 -0400

    deploy

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const models = require('../models')
 
+
 const userController = {}
 
 userController.signUp = async (req, res) => {
